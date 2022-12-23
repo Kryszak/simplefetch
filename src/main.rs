35fe2cce46ad113e77fa0874a70446f4a9b6bc4f
Kryszak <13157331+Kryszak@users.noreply.github.com>
2022-12-23 12:58:08 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -29,32 +29,6 @@ fn main() {
         OsShell::get(),
         OsTerminal::get(),
     ]
-        .into_iter()
-        .flatten()
-        .map(formatting_clojure)
-        .for_each(|info| println!("{}", info));
-}
-
-
-fn main() {
-    let settings = ArgParser::parse_settings();
-    let formatter: Box<dyn Formatter> = match settings.style {
-        FormattingStyle::Left | FormattingStyle::Right => {
-            Box::new(LabelFormatter::new(settings.label_padding, settings.style))
-        }
-        FormattingStyle::Icon => Box::new(IconFormatter::new()),
-    };
-
-    let formatting_clojure = |(icon, label, info)| formatter.format_information(icon, label, info);
-
-    [
-        OsName::get(),
-        OsKernelVersion::get(),
-        OsInstallationDate::get(),
-        OsWm::get(),
-        OsShell::get(),
-        OsTerminal::get(),
-    ]
     .into_iter()
     .flatten()
     .map(formatting_clojure)
