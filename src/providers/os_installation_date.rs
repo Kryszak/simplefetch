f78792d00aad57e44144a4dc5719811b83b0c34e
Kryszak <prajskrzysztof@gmail.com>
2026-08-03 19:28:21 +0200

    replace chrono with jiff

--- src/providers/os_installation_date.rs
+++ src/providers/os_installation_date.rs
@@ -1,5 +1,6 @@
+use jiff::Zoned;
+
 use crate::providers::OsInformation;
-use chrono::{DateTime, Local};
 use std::fs;
 
 pub struct OsInstallationDate {}
@@ -8,8 +9,8 @@ impl OsInformation for OsInstallationDate {
     fn get() -> Option<(String, String, String)> {
         fs::metadata("/")
             .and_then(|m| m.created())
-            .map(|created| -> DateTime<Local> { DateTime::from(created) })
-            .map(|date| format!("{}", date))
+            .map(|created| Zoned::try_from(created).unwrap())
+            .map(|date| date.strftime("%Y-%m-%d %H:%M:%S %:z").to_string())
             .ok()
             .map(|info| (String::from("\u{1F680}"), String::from("Installed"), info))
     }
